01788f3d4d6e6e5b8da74d1975b3c85e11e6035d
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2018-12-19 11:14:07 -0500

    Update simpleBank.test.js

--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -1,3 +1,12 @@
+/*
+
+This test file has been updated for Truffle version 5.0. If your tests are failing, make sure that you are
+using Truffle version 5.0. You can check this by running "trufffle version"  in the terminal. If version 5 is not
+installed, you can uninstall the existing version with `npm uninstall -g truffle` and install the latest version (5.0)
+with `npm install -g truffle`.
+
+*/
+
 var SimpleBank = artifacts.require("./SimpleBank.sol");
 
 contract('SimpleBank', function(accounts) {
@@ -5,7 +14,7 @@ contract('SimpleBank', function(accounts) {
   const owner = accounts[0]
   const alice = accounts[1];
   const bob = accounts[2];
-  const deposit = web3.toBigNumber(2);
+  const deposit = web3.utils.toBN(2);
 
   it("mark addresses as enrolled", async () => {
     const bank = await SimpleBank.deployed();
@@ -22,21 +31,18 @@ contract('SimpleBank', function(accounts) {
   it("should deposit correct amount", async () => {
     const bank = await SimpleBank.deployed();
 
+    await bank.enroll({from: alice});
     await bank.enroll({from: bob});
 
-    await bank.deposit({from: alice, value: deposit});
+    var result = await bank.deposit({from: alice, value: deposit});
     const balance = await bank.balance({from: alice});
+
     assert.equal(deposit.toString(), balance, 'deposit amount incorrect, check deposit method');
 
     const expectedEventResult = {accountAddress: alice, amount: deposit};
 
-    const LogDepositMade = await bank.LogDepositMade();
-    const log = await new Promise(function(resolve, reject) {
-        LogDepositMade.watch(function(error, log){ resolve(log);});
-    });
-
-    const logAccountAddress = log.args.accountAddress;
-    const logDepositAmount = log.args.amount.toNumber();
+    const logAccountAddress = result.logs[0].args.accountAddress;
+    const logDepositAmount = result.logs[0].args.amount.toNumber();
 
     assert.equal(expectedEventResult.accountAddress, logAccountAddress, "LogDepositMade event accountAddress property not emitted, check deposit method");
     assert.equal(expectedEventResult.amount, logDepositAmount, "LogDepositMade event amount property not emitted, check deposit method");
@@ -46,19 +52,14 @@ contract('SimpleBank', function(accounts) {
     const bank = await SimpleBank.deployed();
     const initialAmount = 0;
    
-    await bank.withdraw(deposit, {from: alice});
+	var result = await bank.withdraw(deposit, {from: alice});
     const balance = await bank.balance({from: alice});
 
     assert.equal(balance.toString(), initialAmount.toString(), 'balance incorrect after withdrawal, check withdraw method');
 
-    const LogWithdrawal = await bank.LogWithdrawal();
-    const log = await new Promise(function(resolve, reject) {
-      LogWithdrawal.watch(function(error, log){ resolve(log);});
-    });
-    
-    const accountAddress = log.args.accountAddress;
-    const newBalance = log.args.newBalance.toNumber();
-    const withdrawAmount = log.args.withdrawAmount.toNumber();
+    const accountAddress = result.logs[0].args.accountAddress;
+    const newBalance = result.logs[0].args.newBalance.toNumber();
+    const withdrawAmount = result.logs[0].args.withdrawAmount.toNumber();
 
     const expectedEventResult = {accountAddress: alice, newBalance: initialAmount, withdrawAmount: deposit};
 
