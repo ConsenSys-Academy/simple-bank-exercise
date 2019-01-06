972adda85d4557b4df36739f3321baffb4b59388
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-01-06 10:56:06 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var SimpleBank = artifacts.require("./SimpleBank.sol");
-//var Amazon = artifacts.require("./Amazon.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleBank);
-  //deployer.deploy(Amazon);
 };
