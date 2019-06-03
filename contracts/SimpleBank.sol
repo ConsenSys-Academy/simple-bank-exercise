899ca756690e691732b60d58f0a31eac37e68233
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-06-03 12:20:28 -0400

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -49,7 +49,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() external {
+    function() external payable {
         revert();
     }
 
