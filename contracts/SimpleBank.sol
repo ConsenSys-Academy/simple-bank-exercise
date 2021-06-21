9a529c589bb2c6e3a94f896cdf29dafdea31a2f9
cds-amal <cds.sudama@gmail.com>
2021-06-21 12:54:32 -0400

    Update README

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,33 +1,32 @@
 /*
-    This exercise has been updated to use Solidity version 0.8.5
-    See the latest Solidity updates at
-    https://solidity.readthedocs.io/en/latest/080-breaking-changes.html
-*/
+ * This exercise has been updated to use Solidity version 0.8.5
+ * See the latest Solidity updates at
+ * https://solidity.readthedocs.io/en/latest/080-breaking-changes.html
+ */
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.16 <0.9.0;
 
 contract SimpleBank {
 
-    //
-    // State variables
-    //
+    /* State variables
+     */
     
-    // Fill in the keyword. 
+    
+    // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
     mapping (address => uint) balances ;
     
-    // Fill in the keyword. 
+    // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
-    // to see if a user is enrolled.
+    //       to see if a user is enrolled.
     mapping (address => bool) enrolled;
 
-    // Let's make sure everyone knows who owns the bank. 
-    // Hint: Use the appropriate keyword for this
+    // Let's make sure everyone knows who owns the bank, yes, fill in the
+    // appropriate visilibility keyword
     address owner = msg.sender;
     
-    //
-    // Events - publicize actions to external listeners
-    //
+    /* Events - publicize actions to external listeners
+     */
     
     // Add an argument for this event, an accountAddress
     event LogEnrolled();
@@ -39,9 +38,8 @@ contract SimpleBank {
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
     event LogWithdrawal();
 
-    //
-    // Functions
-    //
+    /* Functions
+     */
 
     // Fallback function - Called if other functions don't match call or
     // sent ether without data
@@ -54,10 +52,10 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    // A SPECIAL KEYWORD prevents function from editing state variables;
-    // allows function to run locally/off blockchain
     function getBalance() public returns (uint) {
-      // 1. Get the balance of the sender of this transaction
+      // 1. A SPECIAL KEYWORD prevents function from editing state variables;
+      //    allows function to run locally/off blockchain
+      // 2. Get the balance of the sender of this transaction
     }
 
     /// @notice Enroll a customer with the bank
@@ -69,23 +67,23 @@ contract SimpleBank {
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    // Add the appropriate keyword so that this function can receive ether
-    // Use the appropriate global variables to get the transaction sender and value
-    // Emit the appropriate event    
-    // Users should be enrolled before they can make deposits
-    function deposit() public payable returns (uint) {
-      // 1. Add the amount to the user's balance, 
+    function deposit() public returns (uint) {
+      // 1. Add the appropriate keyword so that this function can receive ether
+    
+      // 2. Users should be enrolled before they can make deposits
 
-      // 2. Emit the appropriate event associated with this function
+      // 3. Add the amount to the user's balance. Hint: the amount can be
+      //    accessed from of the global variable `msg`
 
-      // 3. return the balance of sndr of this transaction
+      // 4. Emit the appropriate event associated with this function
+
+      // 5. return the balance of sndr of this transaction
     }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    // Emit the appropriate event    
     function withdraw(uint withdrawAmount) public returns (uint) {
       // If the sender's balance is at least the amount they want to withdraw,
       // Subtract the amount from the sender's balance, and try to send that amount of ether
--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -1,13 +1,3 @@
-/*
-
-The public version of the file used for testing can be found here: https://gist.github.com/ConsenSys-Academy/ce47850a8e2cba6ef366625b665c7fba
-
-This test file has been updated for Truffle version 5.0. If your tests are failing, make sure that you are
-using Truffle version 5.0. You can check this by running "trufffle version"  in the terminal. If version 5 is not
-installed, you can uninstall the existing version with `npm uninstall -g truffle` and install the latest version (5.0)
-with `npm install -g truffle`.
-
-*/
 const { catchRevert } = require("./exceptionsHelpers.js");
 var SimpleBank = artifacts.require("./SimpleBank.sol");
 
