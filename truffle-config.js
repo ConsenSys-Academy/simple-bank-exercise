5ec4e0bd32feecef0a72e52070dc4aed3b5dfae2
Matthieu SCARSET <m@matthieuscarset.com>
2023-12-07 17:33:30 +0100

    Compile with correct versions

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,14 @@
 module.exports = {
   networks: {
-      development: {
-          host: 'localhost',
-          port: 8545,
-          network_id: '*'
-      }
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*'
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8",
+    }
   }
-  // // To run contract with the latest compiler, uncomment lines 10-14 below:
-  // compilers: { 
-  //   solc: {
-  //     version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
-  //   }
-  // }
 };
